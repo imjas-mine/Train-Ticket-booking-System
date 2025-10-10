53836d428b574b116b74271b4799050d07410f9b
jasmine <116243241+imjas-mine@users.noreply.github.com>
2025-10-10 02:30:44 -0400

    created userbooking service which maps the data from the file to the list of users

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,10 +1,22 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
 
+import java.io.File;
+import java.util.List;
+
 public class UserBookingService {
+    private static final String USERS_PATH = "../localdb/users.json";
     private User user;
-    public UserBookingService(User user) {
+    private ObjectMapper mapper = new ObjectMapper();
+    private List<User> users;
+
+    public UserBookingService(User user) throws Exception {
         this.user = user;
+        File file = new File(USERS_PATH);
+        users = mapper.readValue(file, new TypeReference<List<User>>() {
+        });
     }
 }
